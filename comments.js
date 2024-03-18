813c2deb388fe8d04db716fe9a22f99ced42265c
German Francisco Cruz Gutierrez <139584349+GermanFrancis@users.noreply.github.com>
2024-03-18 19:54:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,9 @@
-// Create a web server
+// Create web server
+
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var comments = [];
-var server = http.createServer(function (request, response) {
-    var parseObj = url.parse(request.url, true);
-    var pathName = parseObj.pathname;
-    if (pathName === '/') {
-        fs.readFile('./index.html', function (err, data) {
-            if (err) {
-                return response.end('404 Not Found');
-            }
-            response.end(data);
-        });
-    }
-});
+
+
+
 
